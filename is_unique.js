2a1145e1c39842e4c82ea620171efeca4ec0df96
Elton Cheng <eltoncheng86@gmail.com>
2016-05-21 15:28:43 -0400

    Function that checks if all characters in a string is unique

---
+++ is_unique.js
@@ -0,0 +1,20 @@
+/*
+Write a function that checks of characters within a string repeats
+*/
+
+let isUnique = (string) => {
+  let stringArray = string.split('');
+  let array = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j < string.length; j++) {
+      if (stringArray[i] == stringArray[j]) {
+        array.push(stringArray[i]);
+        break;
+      }
+    }
+  }
+  let answer = (array.length == 0) ? 'All Unique' : `Oops, looks like ${array} repeated`;
+  return answer;
+}
+console.log(isUnique('hello'));
+console.log(isUnique('world'));
