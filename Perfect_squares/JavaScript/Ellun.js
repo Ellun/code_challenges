a42713904ac148e5e743ac6d6833197267597a1a
Elton Cheng <eltoncheng86@gmail.com>
2016-05-24 18:07:49 -0400

    added Ellun's solution to Perfect_squares

---
+++ Perfect_squares/JavaScript/Ellun.js
@@ -0,0 +1,11 @@
+let perfectSquares = (min, max) => {
+  let answersArray = [];
+  for (let i = min; i <= max; i++) {
+    if (Math.sqrt(i) % 1 == 0) {
+      answersArray.push(i);
+    }
+  }
+  return answersArray;
+}
+
+console.log(perfectSquares(1, 36));
--- Permutations/JavaScript/Ellun.js
+++ Permutations/JavaScript/Ellun.js
@@ -1,13 +1,13 @@
 // ========= Ellun's Solution ======== //
 
 let permutationCheck = (string1, string2) => {
-  if (string1.length != string2.length) {
-    return false;
+  if (string1.length != string2.length) { // if strings aren't equal length
+    return false;                         // then they can't be permutations
   }
 
-  let s1Array = string1.split('').sort().join('');
-  let s2Array = string2.split('').sort().join('');
-  return (s1Array === s2Array) ? true : false;
+  let s1Array = string1.split('').sort().join(''); // arranges strings in
+  let s2Array = string2.split('').sort().join(''); // alphabetical order
+  return (s1Array === s2Array) ? true : false; // ternary 
 }
 
 console.log(permutationCheck('abc', 'abcd')); // returns false because of length
