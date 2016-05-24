22a64f19f8a952338ac2110b379373847e74365a
Bryce Krah <bkrah@thomasnet.com>
2016-05-24 15:12:21 -0400

    alternate solution

--- reverse_string.js
+++ reverse_string.js
@@ -2,12 +2,22 @@
 Create a function that reverses a string
 */
 
-let reverseString = (string) => {
-  let reverse = '';
-  for (let i = string.length - 1; i >= 0; i--) {
-    reverse += string[i];
-  }
-  return reverse;
+// let reverseString = (string) => {
+//   let reverse = '';
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reverse += string[i];
+//   }
+//   return reverse;
+// }
+
+// console.log(reverseString('hello world'));
+
+
+// ======== Bryce's solution ========= //
+
+function reverseString(string) {
+	var str = string.split('').reverse().join('');
+	console.log(str);
 }
 
-console.log(reverseString('hello world'));
+reverseString("hello world");
